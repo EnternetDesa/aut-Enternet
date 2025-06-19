ec63dbe886aad0e8d9cac6836f856e2d66cf40ba
aut-Enternet <analista.productos@enternet.cl>
2025-06-19 09:24:40 -0400

    Steps
    -Se agrega primer step em lotes y proveedores

---
+++ src/main/java/page/menuPage/LotesPage.java
@@ -0,0 +1,12 @@
+package page.menuPage;
+
+import io.cucumber.java.en.When;
+
+
+public class LotesPage {
+
+
+    @When("este en el modulo de Lotes")
+    public void esteEnElModuloDeLotes() {
+    }
+}
---
+++ src/main/java/page/menuPage/ProveedoresPage.java
@@ -0,0 +1,11 @@
+package page.menuPage;
+
+import io.cucumber.java.en.When;
+
+
+public class ProveedoresPage {
+
+    @When("este en el modulo de proveedores")
+    public void esteEnElModuloDeProveedores() {
+    }
+}
